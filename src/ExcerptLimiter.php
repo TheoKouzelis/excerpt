d9680b9fc6c110abd9c06783fbf59627d406f2fc
Theo Kouzelis <theo@theo.codes>
2015-03-18 01:18:16 +0000

    Add First PHPUnit Test

---
+++ src/ExcerptLimiter.php
@@ -0,0 +1,5 @@
+<?php namespace Kouz;
+
+class ExcerptLimiter
+{
+}
---
+++ tests/ExcerptLimiterTest.php
@@ -0,0 +1,9 @@
+<?php 
+
+class ExcerptLimiterTest extends PHPUnit_Framework_TestCase
+{
+    public function test_create_excerpt()
+    {
+        $limiter = new Kouz\ExcerptLimiter();
+    }
+}
